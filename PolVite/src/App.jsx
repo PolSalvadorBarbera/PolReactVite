75596a260fec5b3524953df28d4adb2b5f19a51a
PolSalvadorBarbera <polsalvadorb@gmail.com>
2025-11-09 21:28:36 +0100

    Fet els productes falta img i footer

--- PolVite/src/App.jsx
+++ PolVite/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header/Header.jsx'
 import Nav from './components/Nav//Nav.jsx'
 import HeroSelection from './components/HeroSelection/HeroSelection.jsx'
+import Tabs from './components/Tabs/Tabs.jsx'
+import topProducts, { allProducts } from './data/products.js'
+import ProductGrid from './components/Product/ProductGrid.jsx'
+
+
 
 
 function App() {
@@ -16,6 +21,17 @@ function App() {
         <Nav/>
         
         <HeroSelection/>
+
+        {/* Selector de productes "destacats" fem crida a ProductGrid*/}
+        <section className="container mx-auto px-4 py-8">
+          <Tabs tabs={["New", "Promo", "Best Seller"]} />
+          <ProductGrid products={topProducts} />
+        </section>
+
+        <section className="container mx-auto px-4 py-8">
+          <h2 className="text-3xl font-bold mb-6 text-black">All Products</h2>
+          <ProductGrid products={allProducts} />
+        </section>
 
       </div>
     </>
--- PolVite/src/components/Tabs/Tabs.jsx
+++ PolVite/src/components/Tabs/Tabs.jsx
@@ -7,16 +7,18 @@ const Tabs = ({ tabs }) => {
     <div className="flex space-x-6 mb-8 border-b border-gray-700">
       {tabs.map((tab) => ( // Itera sobre les pestanyes rebudes com a props
         <button
-          key={tab} // Clau única per a cada pestanya
+          key={tab} // Clau única per a cada pestanya perquè no doni errors
+
           className={`pb-3 text-lg font-semibold transition-colors duration-300
             ${activeTab === tab // Es comprova si la pestanya està activa per aplicar els colors corresponents
               ? 'text-red-600 border-b-2 border-red-600'
               : 'text-gray-400 hover:text-white'
             }`}
+
           onClick={() => setActiveTab(tab)} // Actualitza l'estat quan es fa clic en una pestanya
         >
           {tab}
-        </button>
+        </button> // Botó per a cada pestanya
       ))}
     </div>
   );
